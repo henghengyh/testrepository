5546290a2997ab86107dcfcfe06e60c8b06d2721
henghengyh <yihenglee6@gmail.com>
2025-03-14 23:42:08 +0800

    Update TestFile.java

--- TestFile.java
+++ TestFile.java
@@ -1,10 +1,14 @@
 class TestFile {
   int i;
   public TestFile() {
-    this.i = i;
+    this.i = 1;
   }
 
   public void show() {
     System.out.println(this.i);
   }
+
+  public static void main(String[] args) {
+    TestFile a = new TestFile();
+    a.show();
 }
